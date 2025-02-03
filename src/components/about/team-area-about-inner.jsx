5e488a4462766571d6b58d60bcf4f6738e2c4a46
J P <142513919+xcodeworld@users.noreply.github.com>
2025-02-03 15:12:40 +0530

    Update team-area-about-inner.jsx

--- src/components/about/team-area-about-inner.jsx
+++ src/components/about/team-area-about-inner.jsx
@@ -61,7 +61,7 @@ const Team_Area_About_Inner = () => {
                                 <div className="team-name-position">
                                     <h6 className="font-size-1-22">
                                         <a href="team-details.html">
-                                            Jerome C. Ramirez
+                                            Jayendra Parmar
                                         </a>
                                     </h6>
                                     <span className="font-size-1-16">
@@ -106,11 +106,11 @@ const Team_Area_About_Inner = () => {
                                 <div className="team-name-position">
                                     <h6 className="font-size-1-22">
                                         <a href="team-details.html">
-                                            David George{' '}
+                                            Malla Prakash{' '}
                                         </a>
                                     </h6>
                                     <span className="font-size-1-16">
-                                        Senior Manager
+                                       Software Developer
                                     </span>
                                 </div>
                             </div>
@@ -150,15 +150,15 @@ const Team_Area_About_Inner = () => {
                                 </div>
                                 <div className="team-name-position">
                                     <h6 className="font-size-1-22">
-                                        <a href="team-details.html">Matthew </a>
+                                        <a href="team-details.html">Pravin Parmar </a>
                                     </h6>
                                     <span className="font-size-1-16">
-                                        Web Designer
+                                        Motion Graphic Desinger
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-3 col-lg-3 col-md-6">
+{/*                         <div className="col-xl-3 col-lg-3 col-md-6">
                             <div className="team-inner team-inner-about-page team-inner-about-page-4">
                                 <img src={asset.about_team_4} alt="VRE" />
                                 <div className="team-social-media">
@@ -190,8 +190,8 @@ const Team_Area_About_Inner = () => {
                                             </a>
                                         </li>
                                     </ul>
-                                </div>
-                                <div className="team-name-position">
+                                </div> */}
+{/*                                 <div className="team-name-position">
                                     <h6 className="font-size-1-22">
                                         <a href="team-details.html">
                                             Patrick M. Piazza
@@ -200,7 +200,7 @@ const Team_Area_About_Inner = () => {
                                     <span className="font-size-1-16">
                                         Junior manager
                                     </span>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                     </div>
